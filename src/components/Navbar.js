f257d8cf6b52213eb518dc2e9d0f49c1ad30eb8f
akshat360 <akshataggarwal360@gmail.com>
2021-01-30 17:51:57 +0530

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class=" c-navbar__nav-item nav-item active">
+          <li class=" c-navbar__nav-item nav-item ">
             <a class="nav-link" href="/">
               SV1 Elerctric Scooter <span class="sr-only">(current)</span>
             </a>
