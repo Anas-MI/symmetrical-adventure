73217f61606421f53125e4833aa86dde24ab03d0
akshat360 <akshataggarwal360@gmail.com>
2021-01-30 17:38:28 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import logo1 from './assets/img/logo1.png'
-import { ArrowLeftOutlined } from '@ant-design/icons'
-import FormCard from './components/formCard'
+import React, { useEffect, useState } from 'react';
+import logo1 from './assets/img/logo1.png';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import FormCard from './components/formCard';
+import ShoppingCard from './components/ShoppingCard';
+import DeliveryCard from './components/DeliveryCard';
 import { getAPIkeys } from './utils/stripe';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import Navbar from './components/Navbar';
 
 const App = () => {
-
-  const [stripePromise, setstripePromise] = useState(loadStripe('pk_test_1XPhRvBHCislCDxFaYm3HR97'));
+  const [stripePromise, setstripePromise] = useState(
+    loadStripe('pk_test_1XPhRvBHCislCDxFaYm3HR97')
+  );
 
   const [config, setconfig] = useState({
-    publishableKey: "pk_test_1XPhRvBHCislCDxFaYm3HR97",
-    productID: "",
-    priceID: "",
-    PreOrderPriceId: ""
+    publishableKey: 'pk_test_1XPhRvBHCislCDxFaYm3HR97',
+    productID: '',
+    priceID: '',
+    PreOrderPriceId: '',
   });
 
   const getConfigKeys = async () => {
-    const result = await getAPIkeys()
+    const result = await getAPIkeys();
     if (result) {
-      setconfig(result)
-      setstripePromise(loadStripe(result.publishableKey))
+      setconfig(result);
+      setstripePromise(loadStripe(result.publishableKey));
     }
-  }
+  };
 
   useEffect(() => {
     getConfigKeys();
-  }, [])
-
+  }, []);
 
   return (
     <React.Fragment>
+      <Navbar />
       <section id="into-section">
         <div className="row section-row">
-          <div className="col-lg-6 left-side">
+          {/* <div className="col-lg-6 left-side">
             <div className="d-flex justify-content-around align-items-center h-100 flex-column">
               <div className="d-flex flex-column">
                 <div className="d-flex justify-content-center align-items-center">
                   <ArrowLeftOutlined style={{ color: '#A7A8AC' }} />
-                  <img src={logo1} width="30" alt="logo" style={{ margin: '0 16px' }} />
-                  <p className="text-uppercase fw-bold m-0"
-                    style={{ fontSize: '10px', fontWeight: '600', color: '#8D5719', borderRadius: '4px', background: '#F8E09C', padding: '3px 9px' }}>
+                  <img
+                    src={logo1}
+                    width="30"
+                    alt="logo"
+                    style={{ margin: '0 16px' }}
+                  />
+                  <p
+                    className="text-uppercase fw-bold m-0"
+                    style={{
+                      fontSize: '10px',
+                      fontWeight: '600',
+                      color: '#8D5719',
+                      borderRadius: '4px',
+                      background: '#F8E09C',
+                      padding: '3px 9px',
+                    }}
+                  >
                     Test mode
                   </p>
                 </div>
@@ -54,14 +72,20 @@ const App = () => {
               <div className="justify-content-center align-items-center w-100 copyrights">
                 <p style={{ color: '#626367', fontSize: '12px' }}>
                   Powered by
-                  <span style={{ fontWeight: '600' }}>
-                    stripe
-                  </span>
+                  <span style={{ fontWeight: '600' }}>stripe</span>
                 </p>
-                <p style={{ color: '#626367' }} className="ml-2 mr-2">|</p>
-                <p style={{ color: '#626367', fontSize: '12px' }}>Terms Privacy</p>
+                <p style={{ color: '#626367' }} className="ml-2 mr-2">
+                  |
+                </p>
+                <p style={{ color: '#626367', fontSize: '12px' }}>
+                  Terms Privacy
+                </p>
               </div>
             </div>
+          </div> */}
+          <div className="col-lg-6 left-side">
+            <ShoppingCard />
+            <DeliveryCard />
           </div>
 
           <div className="col-lg-6 right-side">
@@ -78,17 +102,17 @@ const App = () => {
           <div className="d-flex justify-content-center align-items-center w-100">
             <p style={{ color: '#626367', fontSize: '12px' }}>
               Powered by
-              <span style={{ fontWeight: '600' }}>
-                stripe
-              </span>
+              <span style={{ fontWeight: '600' }}>stripe</span>
             </p>
-            <p style={{ color: '#626367' }} className="ml-2 mr-2">|</p>
+            <p style={{ color: '#626367' }} className="ml-2 mr-2">
+              |
+            </p>
             <p style={{ color: '#626367', fontSize: '12px' }}>Terms Privacy</p>
           </div>
         </div>
       </section>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/components/ShoppingCard.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Card } from 'antd';
+import { ShoppingExtraIcon } from '../assets/Icons/CustomIcons';
+import BK_1 from '../assets/img/BK_1.png';
+export default function ShoppingCard() {
+  return (
+    <Card
+      title={<div className="c-card__title">Shopping Cart</div>}
+      bordered={false}
+      extra={<ShoppingExtraIcon />}
+      className="c-card border-0"
+    >
+      <div className="row c-card__list">
+        <div className="col-7">Product Info</div>
+        <div className="col-3">Quantity</div>
+        <div className="col-2 text-right">Price</div>
+      </div>
+      <ShoppingCardItem
+        name={'SV1 Electric Scooter'}
+        image={BK_1}
+        cost={'$69/mo.'}
+      />
+      <ShoppingCardItem name={'Beyond +'} image={''} cost={'$0/mo.'} />
+      <div className="row c-card__footer">
+        <div className="col-7"></div>
+        <div className="col-3">
+          <span className="c-card__footer-subtotal">subtotal </span>
+          <span className="c-card__footer-items">2 items</span>
+        </div>
+        <div className="col-2  text-right">$79/mo.</div>
+      </div>
+    </Card>
+  );
+}
+
+const ShoppingCardItem = (props) => {
+  return (
+    <div className="row c-card__item">
+      <div className="col-2">
+        <img
+          src={
+            props.image !== ''
+              ? props.image
+              : 'https://media.istockphoto.com/photos/background-blue-gradient-abstract-picture-id910021106?k=6&m=910021106&s=612x612&w=0&h=dJ1HiBOr6KTiE5c1odNULI8MbYdpDSZAZD_uS_NEGHQ='
+          }
+          alt={props.name}
+          height={50}
+        />
+      </div>
+      <div className="col-5">{props.name}</div>
+      <div className="col-3">
+        <InputNum />
+      </div>
+      <div className="col-2 text-right">{props.cost}</div>
+    </div>
+  );
+};
+
+const InputNum = (props) => {
+  const [value, setValue] = useState(1);
+  return (
+    <div className="c-input-num row">
+      <span className="col-4" onClick={() => setValue(value - 1)}>
+        -
+      </span>
+      <span className="col-4">{value}</span>
+      <span className="col-4" onClick={() => setValue(value + 1)}>
+        +
+      </span>
+    </div>
+  );
+};
